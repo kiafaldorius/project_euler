74087d057fecf172cc62b3bbdc94bf6e4d7b6a93
Hong <hong.quach@abigfisch.com>
2010-06-13 17:35:28 -0700

    Problem 27

---
+++ problems/21-30/27.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+def euler_27(maxa = 1000, maxb = 1000)
+  prod = 0
+  cons = 0
+  # we resort to the Seive of Erastosthenes for a cheap .is_prime?
+  # however, since we do not know the max prime, [maxa, maxb].max is only a rough estimate
+  # a large array slows down the is_prime? (primes.include?(num)) which is the primary bottleneck
+  primes = (2..[maxa, maxb].max).to_a
+  (2..Math.sqrt([maxa, maxb].max).to_i).each do |i|
+    primes.delete_if { |n| n!=i and n%i == 0 }
+  end
+  (-1*maxa..maxa).each do |a|
+    (-1*maxb..maxb).each do |b|
+      n = 0
+      num = n**2 + a*n + b
+      while num > 0 and primes.include?(num) do
+        n+= 1
+        num = n**2 + a*n + b
+      end
+      if n > cons
+        cons = n
+        prod = a*b
+      end
+    end
+  end
+  prod
+end
+
+#  Euler published the remarkable quadratic formula:
+#
+#  n² + n + 41
+#
+#  It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 40^(2) + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+#
+#  Using computers, the incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, −79 and 1601, is −126479.
+#
+#  Considering quadratics of the form:
+#
+#      n² + an + b, where |a| < 1000 and |b| < 1000
+#
+#      where |n| is the modulus/absolute value of n
+#      e.g. |11| = 11 and |−4| = 4
+#
+#  Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.
+
